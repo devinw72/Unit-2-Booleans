35efd31d9478a65f283c8a8d8bdb17da1766d70f
devinw72 <devinw72@nycstudents.net>
2026-09-17 12:38:09 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+/* const x=18
+if(x>65) {
+    console.log ("Senior Discount");
+}
+ */
+/* !== not true */
+/* const isLoggedIn=true; 
+const role="admin"
+if(isLoggedIn){
+    console.log(isLoggedIn);
+    if (role==="admin") {
+        console.log ("Mod status");
+    } else {
+        console.log("cannot remove posts");
+    }
+
+} */
+let age=35
+let citizen=true
+if (age>=35 && citizen===true) {
+    console.log("Run for President");
+}
+if (haveKey|| isMagic) {
+ console.log("open door");
+}
+function classifyNumber(num) {
+   
+
