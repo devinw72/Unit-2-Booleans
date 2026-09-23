f14a2def0ae89b46c583762bde70bf3f413b9423
devinw72 <devinw72@nycstudents.net>
2026-09-23 12:23:33 -0400

    Update app.js

--- app.js
+++ app.js
@@ -85,4 +85,10 @@ console.log(fizzBuzz(15))
 console.log(fizzBuzz(5))
 console.log(fizzBuzz(3))
 console.log(fizzBuzz(14)) */
-function getShippingCost()
+function getShippingCost(weight,IsMember) {
+    if (IsMember===true && weight<=5)
+        return 
+
+        
+        
+}
